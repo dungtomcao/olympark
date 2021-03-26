da159b42f5947139919e8b18b879941a94a6c6de
dungtomcao <tomcao090@gmail.com>
2021-03-25 18:59:01 -0700

    Create views

--- parkapp/urls.py
+++ parkapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('plants/', views.plants, name='plants'),
+    path('animals/', views.animals, name='animals'),
+    path('habitats/', views.habitats, name='habitats'),
 ]
--- parkapp/views.py
+++ parkapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from datetime import datetime
+from .models import Plant, Animal, Habitat
 # Create your views here.
 def index(request):
     return render(request, 'parkapp/index.html')
+
+def plants(request):
+    plant_list=Plant.objects.all()
+    return render(request, 'parkapp/plants.html', {'plant_list' : plant_list})
+
+def animals(request):
+    animal_list=Animal.objects.all()
+    return render(request, 'parkapp/animals.html', {'animal_list' : animal_list})
+
+def habitats(request):
+    habitat_list=Habitat.objects.all()
+    return render(request, 'parkapp/habitats.html', {'habitat_list' : habitat_list})
